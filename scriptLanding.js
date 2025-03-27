b6f51cd5958bf332b1f766cfe67386ff557b5127
Ayush Nagar <ayushnagar15tfe@gmail.com>
2025-03-27 10:44:37 +0530

    feat: add landing page script to set cookies

---
+++ scriptLanding.js
@@ -0,0 +1,18 @@
+let originalReferrer = decodeURIComponent(
+  document.cookie
+    ?.split(";")
+    .find((cookie) => {
+      return cookie.includes("original_referrer");
+    })
+    ?.split("=")[1] ?? ""
+);
+
+const currentReferrer = document.referrer;
+
+// Add cookie if first time visitor on any link containing garden.finance
+if (!originalReferrer) {
+  originalReferrer = currentReferrer === "" ? "Direct" : currentReferrer;
+  document.cookie = `original_referrer=${encodeURIComponent(
+    originalReferrer
+  )}; domain=.garden.finance; path=/`;
+}
