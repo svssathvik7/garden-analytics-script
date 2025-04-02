6ce2aaeef7d1d7cd59adf072acec70ef539d86d9
Ayush Nagar <ayushnagar15tfe@gmail.com>
2025-04-02 10:10:57 +0530

    fix: for landing page - remove old cookie 'original_referrer' which contains 'garden.finance'

--- scriptLanding.js
+++ scriptLanding.js
@@ -7,6 +7,12 @@ let originalReferrer = decodeURIComponent(
     ?.split("=")[1] ?? ""
 );
 
+// Remove old original_referrer from cookie if it contains "garden.finance"
+if (originalReferrer?.includes("garden.finance")) {
+  document.cookie = `original_referrer=; domain=.garden.finance; path=/; max-age=0`;
+  originalReferrer = null;
+}
+
 const currentReferrer = document.referrer;
 
 // Add cookie if first time visitor on any link containing garden.finance
@@ -14,5 +20,5 @@ if (!originalReferrer) {
   originalReferrer = currentReferrer === "" ? "Direct" : currentReferrer;
   document.cookie = `original_referrer=${encodeURIComponent(
     originalReferrer
-  )}; domain=.garden.finance; path=/; max-age=${60*60*24*180}`;
+  )}; domain=.garden.finance; path=/; max-age=${60 * 60 * 24 * 180}`;
 }
